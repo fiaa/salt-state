360c844538383f2d9d62a2396870a2af903d6011
Jong Shik Lee <aqua@fiaa.net>
2018-12-03 13:40:28 +0900

    Modify the custom grains to support primary net

--- _grains/primary_ip.py
+++ _grains/primary_ip.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 import salt.modules.cmdmod
 
+COMMAND = 'ip route get 1'
 def get_primary_ip():
     grains = {}
-    primary_ip = salt.modules.cmdmod.run('ip route get 1 |awk \'{print $NF;exit}\'', python_shell=True)
-    grains['primary_ip'] = primary_ip
+    output = salt.modules.cmdmod.run(COMMAND, python_shell=True)
+    output = output.split()
+    grains['primary_net'] = output[4]
+    grains['primary_ip'] = output[6]
 
     return grains
